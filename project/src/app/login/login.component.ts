d91154cac5e00cd4a0f37d217d4f3c530454c390
FilipRybinski <rybciaw77@gmail.com>
2023-04-08 02:39:08 +0200

    default view #Do css for login & register

--- project/src/app/login/login.component.ts
+++ project/src/app/login/login.component.ts
@@ -19,7 +19,7 @@ export class LoginComponent implements OnInit{
       password:[,Validators.required]
    })
   }
-  onSubmit(form:FormGroupDirective){
+  onSubmit(){
     if(!this.loginForm.valid){
         return;
     }
--- project/src/app/register/register.component.ts
+++ project/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import {FormBuilder, FormGroup,FormGroupDirective,Validators} from '@angular/forms';
 import { matchValidator } from '../shared/validators/confirmPasswordValidtor';
 import {Countries} from '../shared/models/country.model'
@@ -6,6 +6,7 @@ import { countries } from '../shared/models/countryData.store';
 import { ApiService } from '../shared/api.service';
 import { user } from '../shared/models/user.models';
 import { setServerSideErrors } from '../shared/validators/serverSideValidation';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -14,8 +15,34 @@ import { setServerSideErrors } from '../shared/validators/serverSideValidation';
 export class RegisterComponent implements OnInit{
   registerForm!:FormGroup;
   countriesStore:Countries[]=countries;
-  constructor(private formBuilder:FormBuilder,private api:ApiService){}
+  api =inject(ApiService);
+  formBuilder=inject(FormBuilder);
+  router=inject(Router);
   ngOnInit(): void {
+    this.CreateForm();
+  }
+  onSubmit(){
+    if(!this.registerForm.valid){
+      return;
+    }
+    var user:user={
+      userName:this.registerForm.value.userName,
+      userPassword:this.registerForm.value.userPassword,
+      userPasswordConfirmed:this.registerForm.value.userPasswordConfirmed,
+      email:this.registerForm.value.email,
+      postalDetails:this.registerForm.value.postalDetails,
+      personalData:this.registerForm.value.personalData
+    }
+    this.api.postNewUser(user).subscribe(
+      respone=>{
+        this.router.navigate(['home']);
+      },
+      error=>{
+       setServerSideErrors(error,this.registerForm);
+      }
+    )
+  }
+  CreateForm(){
     this.registerForm=this.formBuilder.group({
       userName:['',Validators.compose([Validators.required,Validators.maxLength(15)])],
       userPassword:['',Validators.compose([Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]),,matchValidator('userPassword',true)],
@@ -33,25 +60,5 @@ export class RegisterComponent implements OnInit{
         phoneNumber:['',Validators.compose([Validators.required,Validators.minLength(9)])]
       })
     })
-  }
-  onSubmit(form:FormGroupDirective){
-    if(!this.registerForm.valid){
-      return;
-    }
-    var user:user={
-      userName:this.registerForm.value.userName,
-      userPassword:this.registerForm.value.userPassword,
-      userPasswordConfirmed:this.registerForm.value.userPasswordConfirmed,
-      email:this.registerForm.value.email,
-      postalDetails:this.registerForm.value.postalDetails,
-      personalData:this.registerForm.value.personalData
-    }
-    this.api.postNewUser(user).subscribe(
-      respone=>{
-      },
-      error=>{
-       setServerSideErrors(error,this.registerForm);
-      }
-    )
   }
 }
