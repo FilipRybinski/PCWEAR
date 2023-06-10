34d27ccb18a7d15498b43be64b7feadb141d1341
Filip Rybiński <rybciaw77@gmail.com>
2023-06-10 13:28:48 +0200

    landing page in progress

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    AOS.init();
+    window.addEventListener('load', AOS.refresh);
+  }
   title = 'project';
 }
--- project/src/app/home/home.component.ts
+++ project/src/app/home/home.component.ts
@@ -1,30 +1,42 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Input, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { NgtArgs, NgtPush, extend, injectNgtLoader } from 'angular-three';
 import * as THREE from 'three';
 import { NgtsOrbitControls } from 'angular-three-soba/controls';
 import { injectNgtsGLTFLoader } from 'angular-three-soba/loaders';
-import { map } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { NgIf } from '@angular/common';
 extend(THREE);
 @Component({
   standalone: true,
   template: `
-    <!-- <ngt-mesh>
-      <ngt-box-geometry />
-      <ngt-mesh-basic-material color="#00ff00" />
-    </ngt-mesh> -->
-    <ngt-point-light [position]="-2" />
-    <ngt-spot-light [position]="2" />
-    <ngt-primitive *args="[model$ | ngtPush]" [scale]="0.2" />
-    <ngts-orbit-controls />
+    <ngt-point-light [position]="5" />
+    <ngt-spot-light [position]="5" />
+    <ng-container *ngIf="model$">
+      <ngt-primitive
+        #mesh
+        *args="[model$ | ngtPush]"
+        [scale]="0.2"
+        [position]="[-1.2, 0.8, 1.5]"
+        (beforeRender)="onBeforeRender($any(mesh))"
+      />
+    </ng-container>
+    <ngts-orbit-controls
+      [enableZoom]="false"
+      [autoRotate]="true"
+      [autoRotateSpeed]="5"
+    />
   `,
-  imports: [NgtArgs, NgtPush, NgtsOrbitControls],
+  imports: [NgtArgs, NgtPush, NgtsOrbitControls, NgIf],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Scene {
-  readonly model$ = injectNgtsGLTFLoader('assets/pcModel/scene.gltf').pipe(
+  readonly model$ = injectNgtsGLTFLoader('assets/pc.glb').pipe(
     map((gltf) => gltf.scene)
   );
+  onBeforeRender(mesh: THREE.Mesh) {
+    mesh.rotation.y = -0.25;
+  }
 }
 @Component({
   selector: 'app-home',
