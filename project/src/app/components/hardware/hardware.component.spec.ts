78d17b50ae6da78b45533536d3927a7bdbc37595
Filip Rybiński <rybciaw77@gmail.com>
2023-08-17 23:00:51 +0200

    few components

---
+++ project/src/app/components/hardware/hardware.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HardwareComponent } from './hardware.component';
+
+describe('HardwareComponent', () => {
+  let component: HardwareComponent;
+  let fixture: ComponentFixture<HardwareComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HardwareComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HardwareComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ project/src/app/components/hardware/hardware.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hardware',
+  templateUrl: './hardware.component.html',
+  styleUrls: ['./hardware.component.scss']
+})
+export class HardwareComponent {
+
+}
