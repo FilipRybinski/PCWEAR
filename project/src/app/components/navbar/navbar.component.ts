55feaf9168e25764ec327748fe000bf0f8aadec9
Filip Rybiński <rybciaw77@gmail.com>
2023-10-12 00:09:21 +0200

    fixed menu on small devices

--- project/src/app/components/navbar/navbar.component.ts
+++ project/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component , ViewChild } from '@angular/core';
+import { Component , ElementRef, ViewChild } from '@angular/core';
 import { AccountService } from 'src/app/services/account.service';
 
 @Component({
@@ -7,11 +7,15 @@ import { AccountService } from 'src/app/services/account.service';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  @ViewChild('check') button!:HTMLInputElement;
+  @ViewChild('check') button!:ElementRef;
   constructor(private _accountService:AccountService){
   }
-  closeMenu() {
-    if(this.button!=null) this.button.checked = false;
+  toggleOpen() {
+    const body=document.querySelector('body');
+    if(this.button!=null && body){
+      this.button.nativeElement.checked=!this.button.nativeElement.checked;
+      body.style.overflow=='hidden' ? body.style.removeProperty('overflow') : body.style.overflow='hidden';
+    }
   }
   logout(){
     this._accountService.logout().subscribe({next:(res)=>this._accountService.currentLoggedUser=res})
